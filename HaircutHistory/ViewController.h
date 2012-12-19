5e512bb32ba81a5f92c45bfcbb0283c33a284742
Jorge.Pardo <Jorge.Pardo@treenovum.es>
2012-12-19 11:59:22 +0100

    Initial Commit

---
+++ HaircutHistory/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HaircutHistory
+//
+//  Created by Jorge Pardo on 19/12/12.
+//  Copyright (c) 2012 MagicDealers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HaircutHistory/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HaircutHistory
+//
+//  Created by Jorge Pardo on 19/12/12.
+//  Copyright (c) 2012 MagicDealers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
